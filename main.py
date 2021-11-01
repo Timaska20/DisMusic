b2a97fcba4eaafb70de9399ea252595efd977750
Timaska <tamirlansuska@gmail.com>
2021-11-01 09:46:30 +0600

    add command next

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import mtoken
 import discord
 from discord.ext import commands
 import youtube_dl
@@ -18,8 +19,9 @@ async def connect(ctx):
     for Voice_Chanel in Voice_Chanels:
         if ctx.author.id in Voice_Chanel.voice_states:
             current_Voice = Voice_Chanel
-    if ctx.author.id not in Music_list:
-        Music_list[ctx.author.id] = []
+    if ctx.guild.id not in Music_list:
+        print(ctx.guild.id)
+        Music_list[ctx.guild.id] = []
     if ctx.voice_client is not None:
         return await ctx.voice_client.move_to(current_Voice)
     await current_Voice.connect()
@@ -28,29 +30,35 @@ async def connect(ctx):
 async def p(ctx, url : str, is_loop=False):
     global ydl_opts, Music_list
     await connect(ctx)
-    if not is_loop:
-        ytdl = youtube_dl.YoutubeDL(ydl_opts)
-        data = ytdl.extract_info(url, download=False)
-        print(data['url'])
-        Music_list[ctx.author.id].append(data['url'])
-        await ctx.send("Music add")
-    else:
-        Music_list[ctx.author.id].append(Music_list[ctx.author.id])
-        await ctx.send("Music looped")
+
+    ytdl = youtube_dl.YoutubeDL(ydl_opts)
+    data = ytdl.extract_info(url, download=False)
+    print(data['url'])
+    Music_list[ctx.guild.id].append(data['url'])
+    await ctx.send("Music add")
     while True:
-        if ctx.voice_client.is_playing():
-            await asyncio.sleep(2)
-            continue
-        else:
-            try:
-                url = Music_list[ctx.author.id].pop()
-                ctx.voice_client.play(discord.FFmpegPCMAudio(url),after=lambda e: print(f'Player error: {e}') if e else None)
-            except IndexError:
-                break
+        try:
+            if ctx.voice_client.is_playing():
+                await asyncio.sleep(2)
+                continue
+            else:
+                try:
+                    url = Music_list[ctx.guild.id].pop()
+                    ctx.voice_client.play(discord.FFmpegPCMAudio(url),after=lambda e: print(f'Player error: {e}') if e else None)
+                except IndexError:
+                    break
+        except AttributeError:
+            break
+
 
 @client.command()
-async def loop(ctx):
-    await p(ctx, "LOOP",True)
+async def next(ctx):
+    if ctx.voice_client.is_playing():
+        if len(Music_list[ctx.guild.id])>0:
+            url = Music_list[ctx.guild.id].pop()
+            ctx.voice_client.source = discord.FFmpegPCMAudio(url)
+
+
 @client.command()
 async def leave(ctx):
     if ctx.voice_client is not None:
@@ -59,4 +67,4 @@ async def leave(ctx):
         await ctx.send("Bot not in voice channel")
 
 
-client.run("ODkxMzU0NTk0NTM2NDgwNzc4.YU9Ipw.E9zxebd2oBDCv-zOa7oLXP06ZeE")
+client.run(mtoken.dev_token)
